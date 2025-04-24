b04f85024fcd99109ba65b8beca9e8ac49212a17
Dilshan841 <rathnayakehemantha427@gmail.com>
2025-04-24 13:45:57 +0530

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,4 +1,31 @@
-global.owner = ['+94772194789']
-global.ownername = 'Dilshan Ashinsa'
-global.botname = 'DILSHAN-MD'
-global.prefix = ['.']
+
+module.exports = {
+  botName: 'DILSHAN-MD',
+  ownerNumber: '94772194789',
+  ownerName: 'Dilshan Ashinsa',
+  prefix: '.',
+  authFolder: './auth_info_dilshanmd',
+  sessionFilePath: './auth_info_dilshanmd/creds.json',
+};
+
+const settings = {
+  ownerName: "Dilshan Ashinsa",
+  admins: ["94772194789"],
+  botName: "DILSHAN-MD",
+  mode: "public",
+  autoRead: false,
+  autoBio: false,
+  antiDelete: true,
+  messages: {
+    success: "✅Done",
+    adminOnly: "false.",
+    botAdmin: "false",
+    ownerOnly: "false",
+    groupOnly: "false",
+    privateOnly: "false",
+    wait: "⏳ කරුණාකර රැදින්න...",
+    error: "❌ ගැටළුවක් ඇතිවුණා!",
+  }
+};
+
+module.exports = settings;
